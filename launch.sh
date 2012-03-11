9e4c04ed04854e66bd4deba4d1c26f8bb3ec8706
Adrian Kosmaczewski <adrian@akosma.com>
2012-03-11 16:52:16 +0100

    Removed unused code

--- launch.sh
+++ launch.sh
@@ -2,8 +2,3 @@
 
 (node editor/app.js & open http://localhost:3000/) | parallel
 
-# cd editor/public/projects
-# rm current
-# ln -s default current
-# cd ../../../
-
--- mac.sh
+++ mac.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env sh
 
-cd editor/public/projects
-rm current
-ln -s default current
-cd ../../../
-
 (node editor/app.js & open /Applications/Teaching\ Editor.app) | parallel
 
